b7a1cbc8f20da5352ae2cbfb77a0c53b14aa73e9
Jackson Rodriguez <jacksonlrodriguezf@gmail.com>
2021-07-01 14:33:47 -0500

    The project is able to successfully import JSON files.

--- resources/views/partials/upload-json.blade.php
+++ resources/views/partials/upload-json.blade.php
@@ -1,9 +1,18 @@
 <div class="row mt-2">
-    <form action="" enctype="multipart/form-data">
+    <form
+        action="{{route('treatments.upload')}}"
+        enctype="multipart/form-data"
+        method="POST"
+    >
+        @csrf
+
+        @include('layout.partials.messages')
+
         <div class="row">
             <div class="col-6">
                 <input
                     type="file"
+                    name="file"
                     class="form-control"
                 >
             </div>
